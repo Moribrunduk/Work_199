8d1c1517fd4acafc18161bd852af691d857f2c12
Moribrunduk <aild19921128@gmali.com>
2022-10-23 18:39:06 +0300

    маленький рефакторинг

--- Main_table.py
+++ Main_table.py
@@ -26,14 +26,16 @@ class MainWidget(QWidget):
         self.data_table_view = QTableView()
         self.model = QStandardItemModel(self)
         
-        item1 = QStandardItem("Лелей")
+    def add_data(self):
         # ДОБАВЛЯЕМ ТАБЕЛЬНЫЕ
 
         #задаем начальный столбец
         x = 0
         work_column = 0
         for tabel in tabels:
+            #создаем итем
             item = QStandardItem(tabel)
+            #делаем его нередактируемым
             item.setEditable(False)
             self.model.setItem(x, work_column, item)
             self.data_table_view.setSpan(x,0,2,1)
@@ -49,6 +51,7 @@ class MainWidget(QWidget):
             item.setEditable(False)
             self.model.setItem(x, work_column, item)
             x=x+2
+
         # ДОБАВЛЯЕМ ИНИЦИАЛЫ
         x = 1
         for tabel in tabels:
@@ -82,24 +85,20 @@ class MainWidget(QWidget):
             for y in range(0,16):
                 self.model.setItem(x, y+work_column, QStandardItem(""))
 
-
+    def parameters(self):
         #Задаем параметры таблицы
         self.data_table_view.setModel(self.model)
         # self.data_table_view.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
         self.data_table_view.horizontalHeader().setMinimumSectionSize(30)
         self.data_table_view.resizeColumnsToContents()
         
-        
-        
-        
-
-
         self.top_layout.addWidget(self.data_table_view)
-        
-
+    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main = MainWidget()
+    main.add_data()
+    main.parameters()
     main.show()
 
     sys.exit(app.exec_())
