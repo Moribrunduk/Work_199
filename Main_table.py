7db551724a6ff0cd23791bd0999e41bed498daef
Moribrunduk <aild19921128@gmali.com>
2022-10-24 20:08:12 +0300

    Добавил рабочий календарь слева

--- Main_table.py
+++ Main_table.py
@@ -1,5 +1,6 @@
 
 import sys
+from tracemalloc import start
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
@@ -27,11 +28,38 @@ class MainWidget(QWidget):
         self.model = QStandardItemModel(self)
         
     def add_data(self):
+
+        #ДОБАВЯЕМ РАБОЧИЙ КАЛЕНДАРЬ
+        #TODO
+        x = 0
+        work_column=2
+        for data in range(1,33):
+            if data <16:
+                item = QStandardItem(str(data))
+                #делаем его нередактируемым
+                item.setEditable(False)
+                self.model.setItem(x, work_column, item)
+                work_column+=1
+            elif data == 16 :
+                item = QStandardItem("-")
+                #делаем его нередактируемым
+                item.setEditable(False)
+                self.model.setItem(x, work_column, item)
+                work_column+=1
+            else:
+                item = QStandardItem(str(data-1))
+                #делаем его нередактируемым
+                item.setEditable(False)
+                self.model.setItem(x+1, work_column-16, item)
+                work_column+=1
+
+
+
         # ДОБАВЛЯЕМ ТАБЕЛЬНЫЕ
 
+        x=1+1
+        work_column =0
         #задаем начальный столбец
-        x = 0
-        work_column = 0
         for tabel in tabels:
             #создаем итем
             item = QStandardItem(tabel)
@@ -45,7 +73,7 @@ class MainWidget(QWidget):
         
         
         # ДОБАВЛЯЕМ ФАМИЛИИ
-        x = 0
+        x = 1+1
         for tabel in tabels:
             item = QStandardItem(tabels[tabel]["фамилия"])
             item.setEditable(False)
@@ -53,7 +81,7 @@ class MainWidget(QWidget):
             x=x+2
 
         # ДОБАВЛЯЕМ ИНИЦИАЛЫ
-        x = 1
+        x = 2+1
         for tabel in tabels:
             item = QStandardItem(tabels[tabel]["инициалы"])
             item.setEditable(False)
@@ -63,7 +91,7 @@ class MainWidget(QWidget):
         work_column+=1
 
         # ДОБАВЛЯЕМ ГРАФИК ОТРАБОТАННЫХ СМЕН
-        x = 0
+        x = 1+1
         for tabel in tabels:
             for day in range(0,len(tabels[tabel]["отработанные смены"])):
                 if day<16:
@@ -80,14 +108,16 @@ class MainWidget(QWidget):
 
         # ДОБАВЛЯЕМ ЯЧЕЙКИ В КОТОРЫЕ БУДЕМ ЗАНОСИТЬ ТАБЕЛЬНЫЕ ЗАМЕЩАЮЩИХ
         
-        x = 0
-        for x in range(0,len(tabels)*2):
+        x = 1+1
+        work_row = x
+        for x in range(work_row,len(tabels)*2):
             for y in range(0,16):
                 item = QStandardItem("")
                 # делаем их все нередактируемые и заполняем цветом
                 item.setEditable(False)
                 item.setBackground(QtGui.QColor(192,192,192))
                 self.model.setItem(x, y+work_column, item)
+        
 
     def add_color_table_he_for_him(self):
 
@@ -96,7 +126,7 @@ class MainWidget(QWidget):
         # НАЧАЛЬНАЯ КОЛОНКа # TODO сделать чтобы она изменялась по всему документу
         work_column = 18
         # НАЧАЛЬНАЯ СТРОКА # TODO сделать чтобы она изменялась по всему документу
-        x = 0
+        x = 1+1
 
         # пробегаемся по табельным
         for tabel in tabels:
@@ -119,14 +149,30 @@ class MainWidget(QWidget):
 
             x=x+2
 
+    def show_info(self):
+        row = self.data_table_view.currentIndex().row()
+        column = self.data_table_view.currentIndex().column()
+        print(f'({row}, {column})')
+        if self.model.index(row,0).data() == None:
+            print(self.model.index(row-1,0).data())
+        else:
+            print(self.model.index(row,0).data())
+
+        data = self.data_table_view.currentIndex().data()
+        print(data)
+                            
     def parameters(self):
         #Задаем параметры таблицы
         self.data_table_view.setModel(self.model)
         # self.data_table_view.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
         self.data_table_view.horizontalHeader().setMinimumSectionSize(30)
         self.data_table_view.resizeColumnsToContents()
-        
+        #Показывае данные при изменении в ячейке
+        self.model.itemChanged.connect(self.show_info)
+        #Показывает данные при клике на ячейку
+        self.data_table_view.clicked.connect(self.show_info)
         self.top_layout.addWidget(self.data_table_view)
+        
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
